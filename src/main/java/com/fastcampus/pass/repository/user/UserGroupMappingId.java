c8c861660cfb4b7dcb116983f56a9d4f331837c4
김준수 <asdg2795@naver.com>
2024-03-22 14:13:31 +0900

    ALL

---
+++ src/main/java/com/fastcampus/pass/repository/user/UserGroupMappingId.java
@@ -0,0 +1,8 @@
+package com.fastcampus.pass.repository.user;
+
+import java.io.Serializable;
+
+public class UserGroupMappingId implements Serializable {
+    private String userGroupId;
+    private String userId;
+}
